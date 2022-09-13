25edcd73151f81696214c21b73b2d189888d3fc1
calvincheng919 <calvin@4mile.io>
2022-09-12 23:11:20 -0700

    jest config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+{
+    "outputDirectory": "reports",
+    "outputName": "jest-junit.xml",
+    "ancestorSeparator": " › ",
+    "uniqueOutputName": "false",
+    "suiteNameTemplate": "{filepath}",
+    "classNameTemplate": "{classname}",
+    "titleTemplate": "{title}"
+}

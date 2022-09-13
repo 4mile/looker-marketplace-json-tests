a72633c3e26fbb0bd56ffe2b6fbcdf2844eb6a2f
calvincheng919 <calvin@4mile.io>
2022-09-13 15:34:40 -0700

    remove jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
     "uniqueOutputName": "false",
     "suiteNameTemplate": "{filepath}",
     "classNameTemplate": "{classname}",
-    "titleTemplate": "{title}",
+    "titleTemplate": "{title}"
 }

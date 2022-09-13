076e33ad31ad5dbfb52e12e05860e16717b6078a
calvincheng919 <calvin@4mile.io>
2022-09-13 15:51:57 -0700

    re add config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "outputDirectory": "reports",
-    "outputName": "jest-junit.xml",
-    "ancestorSeparator": " › ",
-    "uniqueOutputName": "false",
-    "suiteNameTemplate": "{filepath}",
-    "classNameTemplate": "{classname}",
-    "titleTemplate": "{title}",
+    // "outputDirectory": "reports",
+    // "outputName": "jest-junit.xml",
+    // "ancestorSeparator": " › ",
+    // "uniqueOutputName": "false",
+    // "suiteNameTemplate": "{filepath}",
+    // "classNameTemplate": "{classname}",
+    // "titleTemplate": "{title}",
     "rootDir": "/home/runner/work/_actions/4mile/looker-marketplace-json-tests/main"
 }
